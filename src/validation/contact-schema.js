7ba51cc51acb20af325ced61caf6776ceb684882
Yaroslava <yaroslava.hovenko@gmail.com>
2024-07-18 14:45:05 +0300

    fix problem

--- src/validation/contact-schema.js
+++ src/validation/contact-schema.js
@@ -1,13 +1,13 @@
 import Joi from 'joi';
-
 import { typeList } from '../constans/constans.js';
 
 export const contactAddSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().min(3).max(20).email().optional(),
+  // contactType: Joi.string().min(3).max(20),
   isFavourite: Joi.boolean(),
-  type: Joi.string()
+  contactType: Joi.string()
     .min(3)
     .max(20)
     .valid(...typeList),
@@ -17,8 +17,9 @@ export const contactUpdateSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string().min(3).max(20).email().optional(),
+  // contactType: Joi.string().min(3).max(20),
   isFavourite: Joi.boolean(),
-  type: Joi.string()
+  contactType: Joi.string()
     .min(3)
     .max(20)
     .valid(...typeList),
